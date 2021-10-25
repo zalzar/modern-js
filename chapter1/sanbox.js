ac60b8ce9599b20f37bc948eaaf712508ab91774
Ali <ali.zalzar@gail.com>
2021-10-25 09:44:16 +0300

    new lessons

---
+++ chapter1/sanbox.js
@@ -0,0 +1,10 @@
+alert('hello, world');
+
+let age=25;
+let year=2021;
+
+console.log(age);
+console.log(year);
+
+var block=13;
+console.log(block);
--- chapter2/sandbox.js
+++ chapter2/sandbox.js
@@ -1,10 +1,91 @@
-alert('hello, world');
+// //string
+// console.log('hello world');
 
-let age=25;
-let year=2021;
+// let email = 'ali.z@gmail.com';
+// console.log(email);
 
-console.log(age);
-console.log(year);
+// //string concatingation
+// let firstName = 'John';
+// let lastName = 'Doe';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
 
-var block=13;
-console.log(block);
+// //getting charecters
+// console.log(fullName[0]);
+
+// //string length
+// console.log(fullName.length);
+
+// //string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, ' ', fullName);
+
+
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+// let email = 'ali.z@xoload.com';
+// let result0 = email.lastIndexOf('o');
+// let result1 = email.slice(2, 10);
+// let result2 = email.substr(2, 10);
+// let result3 = email.replace('z', 'g');
+// let result4 = email.replace('o', 'a');
+// console.log(result0);
+// console.log(result1, '\n', result2, '\n', result3, '\n', result4);
+
+// let radios = 10;
+// const pi = 3.14;
+// console.log(radios,pi);
+
+//math operators +  -  *  /  **  %
+//remember BIDMAS? no? read the next line
+//BIDMAS = Brackets, Indices, Division/Multiplication, Addition/Subtraction
+
+// let result=(pi*radios**2);
+// console.log(result);
+
+
+// let likes = 10;
+// likes++;
+// console.log(likes);
+// likes--;
+// console.log(likes);
+// likes -= likes;
+// console.log(likes);
+// likes += likes + 10;
+// console.log(likes);
+// likes *= likes;
+// console.log(likes);
+// likes /= likes;
+// console.log(likes);
+
+// let likes=10;
+// let result = 'the blog has '+likes+' likes';
+// console.log(result);
+
+
+// // // *****template string
+// const title = 'best reads of 2021';
+// const author = 'mario';
+// const likes = 30;
+
+// // // *****concatination way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// // // *****template string way
+// let resultA = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log('\n');
+// console.log(resultA);
+
+
+// // // *****creating html template
+// let html = `
+//     <h2>${title}</h2>
+//     <p>${author}</p>
+//     <span>This blog has ${likes} likes</span>
+// `;
+// console.log(html);
+
