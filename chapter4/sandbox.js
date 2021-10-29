be841686c1a6a308def01c81b0f75ada4de27446
Ali <ali.zalzar@gail.com>
2021-10-29 08:18:16 +0300

    new chapter

---
+++ chapter4/sandbox.js
@@ -0,0 +1,9 @@
+console.log('-------------------Functions-------------------');
+const speak = function() {
+    console.log('good day!');
+};
+speak();
+function P(a) {
+    console.log(a);
+}
+P('test');
