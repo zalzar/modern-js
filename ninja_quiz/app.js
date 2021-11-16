e524e49cddc6da5fe277608a011a5a848139f6d1
Ali <ali.zalzar@gail.com>
2021-11-16 17:47:09 +0200

    lesson 68 - Showing the Score

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -1,16 +1,28 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+
+
+const res = document.querySelector('.result');
+
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
     //if(correctAnswers===)
     // check answers
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
-            score += 25;
+            score += 1;
         }
     });
-    console.log(score);
+    
+    res.querySelector('span').textContent = `${(100 / correctAnswers.length) * score} %`;
+    // Another way to do above calculation is 
+    //const span = document.querySelector('p span');
+    //span.textContent = ((100 / correctAnswers.length) * score) + "%";
+
+    res.classList.remove('d-none');
+    
 });
