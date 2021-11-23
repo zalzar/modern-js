e61410c9e9de10ab0bdb14dafe38a9f55aabc8b7
Ali <ali.zalzar@gail.com>
2021-11-23 22:58:26 +0200

    lesson 89 -Making HTTP Requests (XHR)

--- chapter12/sandbox.js
+++ chapter12/sandbox.js
@@ -1,13 +1,33 @@
 console.log('-------------------Chapter 12 - Async JavaScript-------------------');
-// 87. Async Code in Action
-{
-    // the following example makes it easy to understand how Async works 
-    console.log(1);
-    console.log(2);
-    setTimeout(() => {
-        console.log('callback function fired');
-    }, 2000);
-    console.log(3);
-    console.log(4);
-}
+// // 87. Async Code in Action
+// {
+//     // the following example makes it easy to understand how Async works 
+//     console.log(1);
+//     console.log(2);
+//     setTimeout(() => {
+//         console.log('callback function fired');
+//     }, 2000);
+//     console.log(3);
+//     console.log(4);
+// }
 
+// // 88. What are HTTP Requests?
+// {
+//     // https://jsonplaceholder.typicode.com/
+//     // https://jsonplaceholder.typicode.com/todos/1
+// }
+
+// 89. Making HTTP Requests (XHR)
+{
+    const request = new XMLHttpRequest();
+    
+    request.addEventListener('readystatechange',()=>{
+        // console.log(request, request.readyState);
+        if(request.readyState===4){
+            console.log(request.responseText);
+        }
+    });
+ 
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+}
