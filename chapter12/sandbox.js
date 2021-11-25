9fab89fbb56ebc3c8383e50a85be6efe84b24662
Ali <ali.zalzar@gail.com>
2021-11-25 08:09:15 +0200

    lesson 93 - Callback Hell

--- chapter12/sandbox.js
+++ chapter12/sandbox.js
@@ -81,33 +81,66 @@ console.log('-------------------Chapter 12 - Async JavaScript-------------------
 //     console.log(4);
 // }
 
-// 92. JSON Data
+// // 92. JSON Data
+// {
+//     const getTodos = (callback) => {
+//         const request = new XMLHttpRequest();
+
+//         request.addEventListener('readystatechange', () => {
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 callback(undefined,data);
+//             } else if (request.readyState === 4) {
+//                 // if the status is not 200
+//                 callback('could not fetch data',undefined);
+//             }
+//         });
+//         request.open('GET', 'perTab.json');
+//         request.send();
+//     };
+//     console.log(1);
+//     console.log(2);
+
+//     getTodos((error,data)=>{
+//         console.log('callback is fired');
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log(data);
+//         }
+//     });
+//     console.log(3);
+//     console.log(4);
+// }
+
+// 93. Callback Hell (requsting from multiple JSON files)
 {
-    const getTodos = (callback) => {
+    const getTodos = (resource, callback) => {
         const request = new XMLHttpRequest();
 
         request.addEventListener('readystatechange', () => {
             if (request.readyState === 4 && request.status === 200) {
                 const data = JSON.parse(request.responseText);
-                callback(undefined,data);
+                callback(undefined, data);
             } else if (request.readyState === 4) {
                 // if the status is not 200
-                callback('could not fetch data',undefined);
+                callback('could not fetch data', undefined);
             }
         });
-        request.open('GET', 'perTab.json');
+        request.open('GET', resource);
         request.send();
     };
     console.log(1);
     console.log(2);
 
-    getTodos((error,data)=>{
-        console.log('callback is fired');
-        if(error){
-            console.log(error);
-        }else{
+    getTodos('./perTab.json', (error, data) => {
+        console.log(data);
+        getTodos('./perTab2.json', (error, data) => {
             console.log(data);
-        }
+            getTodos('./perTab3.json', (error, data) => {
+                console.log(data);
+            });
+        });
     });
     console.log(3);
     console.log(4);
