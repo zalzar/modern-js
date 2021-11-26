4c6185124e0ca60569b914e4597d1026059cbeba
Ali <ali.zalzar@gail.com>
2021-11-26 23:54:52 +0200

    lesson 94 - Promise Basics + petProject updates

--- chapter12/sandbox.js
+++ chapter12/sandbox.js
@@ -113,35 +113,86 @@ console.log('-------------------Chapter 12 - Async JavaScript-------------------
 //     console.log(4);
 // }
 
-// 93. Callback Hell (requsting from multiple JSON files)
+// // 93. Callback Hell (requsting from multiple JSON files)
+// {
+//     const getTodos = (resource, callback) => {
+//         const request = new XMLHttpRequest();
+
+//         request.addEventListener('readystatechange', () => {
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 callback(undefined, data);
+//             } else if (request.readyState === 4) {
+//                 // if the status is not 200
+//                 callback('could not fetch data', undefined);
+//             }
+//         });
+//         request.open('GET', resource);
+//         request.send();
+//     };
+//     console.log(1);
+//     console.log(2);
+
+//     getTodos('./perTab.json', (error, data) => {
+//         console.log(data);
+//         getTodos('./perTab2.json', (error, data) => {
+//             console.log(data);
+//             getTodos('./perTab3.json', (error, data) => {
+//                 console.log(data);
+//             });
+//         });
+//     });
+//     console.log(3);
+//     console.log(4);
+// }
+
+// 94. Promise Basics
 {
     const getTodos = (resource, callback) => {
-        const request = new XMLHttpRequest();
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
 
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                callback(undefined, data);
-            } else if (request.readyState === 4) {
-                // if the status is not 200
-                callback('could not fetch data', undefined);
-            }
-        });
-        request.open('GET', resource);
-        request.send();
-    };
-    console.log(1);
-    console.log(2);
-
-    getTodos('./perTab.json', (error, data) => {
-        console.log(data);
-        getTodos('./perTab2.json', (error, data) => {
-            console.log(data);
-            getTodos('./perTab3.json', (error, data) => {
-                console.log(data);
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    const data = JSON.parse(request.responseText);
+                    resolve(data);
+                } else if (request.readyState === 4) {
+                    // if the status is not 200
+                    reject('We lost a file: ');
+                }
             });
-        });
+            request.open('GET', resource);
+            request.send();
+        })
+
+    };
+    jsonFiles=['./perTab.json','./perTab2.json','./perTab3.json'];
+    getTodos(jsonFiles[0]).then((data) => {
+        console.log('Promise 1 resolved', data);
+    }).catch(error => {
+        console.log(error,jsonFiles[0]+' broke the promise.',);
     });
-    console.log(3);
-    console.log(4);
+
+
+
+    //promise example
+    const getSomething = () => {
+        return new Promise((resolve, reject) => {
+            //resolve('Some data');
+            reject('Some data');
+        });
+    };
+    // getSomething().then(data=>{ 
+    //     console.log(data);
+    // }),(error)=>{
+    //     console.log(error);
+    // };
+
+    // getSomething().then(data => {
+    //     console.log(data);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
+
+
 }
--- petProject/script.js
+++ petProject/script.js
@@ -1,36 +1,82 @@
-//get refrence to the 'ul'
-const div = document.querySelector('.row');
+// //get refrence to the 'ul'
+// const div = document.querySelector('.row');
 
-let html = ``;
-//const span = ['Mario', 'Luigi'];
-const span = ['Mario', 'Luigi', 'Ali', 'Rali', 'Bella'];
-span.forEach(function (person) {
-    // create html template
-    //html += `${person}`;
-    html += `
-    <div class="col s12 l6">
-    <div class="card">
-        <div class="card-image">
-            <img src="./img/curry.jpg" alt="" srcset="">
-            <a href="" class="halfway-fab btn-floating pink pulse">
-                <i class="material-icons">favorite</i>
-            </a>
+// let html = ``;
+// //const span = ['Mario', 'Luigi'];
+// const span = ['Mario', 'Luigi', 'Ali', 'Rali', 'Bella'];
+// span.forEach(function (person) {
+//     // create html template
+//     //html += `${person}`;
+//     html += `
+//     <div class="col s12 l6">
+//     <div class="card">
+//         <div class="card-image">
+//             <img src="./img/curry.jpg" alt="" srcset="">
+//             <a href="" class="halfway-fab btn-floating pink pulse">
+//                 <i class="material-icons">favorite</i>
+//             </a>
+//         </div>
+//         <div class="card-content">
+//             <span class="card-title">${person}</span>
+//             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+//                 laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+//                 voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+//                 cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+//         </div>
+//         <div class="card-action">
+//             <a href="">More details</a>
+//             <a href="">View Ingredients</a>
+//         </div>
+//     </div>
+// </div>
+//     `;
+// });
+// console.log(html);
+// div.innerHTML = html;
+
+
+const content = document.querySelector('.row');
+const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if (request.readyState === 4) {
+            // if the status is not 200
+            callback('could not fetch data', undefined);
+        }
+    });
+    request.open('GET', resource);
+    request.send();
+};
+getTodos('./perTab.json', (error, data) => {
+    html = ``;
+    console.log(data);
+    data.forEach((element, index) => {
+        html += `
+        <!-- Modal Trigger -->
+        <a class="waves-effect waves-light btn modal-trigger" href="#modal${index + 2}">${element.Name}</a>
+
+        <!-- Modal Structure -->
+        <div id="modal${index + 2}" class="modal bottom-sheet">
+            <div class="modal-content">
+                <h4>h4</h4>
+                <p>A bunch of text</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+            </div>
         </div>
-        <div class="card-content">
-            <span class="card-title">${person}</span>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </div>
-        <div class="card-action">
-            <a href="">More details</a>
-            <a href="">View Ingredients</a>
-        </div>
-    </div>
-</div>
-    `;
+        `;
+    });
+    content.innerHTML = html;
+
 });
-console.log(html);
-div.innerHTML = html;
+document.addEventListener('DOMContentLoaded', () => {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
