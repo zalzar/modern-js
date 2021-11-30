e9bcbf57a2e886841424f66f71222f05edc9e1a4
Ali <ali.zalzar@gail.com>
2021-11-30 08:49:33 +0200

    lesson 101 - AccuWeather API

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,3 @@
+const key = 'VsYFwlGIN8BHbUO4fT1rARo3PEqQQyJQ';
+const CitySearch = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const CurrentConditions = 'http://dataservice.accuweather.com/currentconditions/v1/{locationKey}';
