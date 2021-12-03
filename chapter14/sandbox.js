4ea14e03ecc17f2388bf9954fdd2fd1445024721
Ali <ali.zalzar@gail.com>
2021-12-03 15:51:36 +0200

    lesson         {text: 'Play mariokart', author: 'shaun'},

--- chapter14/sandbox.js
+++ chapter14/sandbox.js
@@ -23,32 +23,45 @@ console.log('-------------------Section 14: Local Storage-------------------');
 //     console.log(Name, age);
 // }
 
-// 112. Deleting Storage Data
+// // 112. Deleting Storage Data
+// {
+//     // store data in local storage
+//     localStorage.setItem('Name', 'Ali');
+//     localStorage.setItem('age', '32');
+
+//     // get data from local storage and
+//     let Name = localStorage.getItem('Name');
+//     let age = localStorage.getItem('age');
+
+//     console.log(Name, age);
+
+//     // updating data
+//     localStorage.setItem('Name', 'Ali Zal');
+
+//     // updating data using dot notation
+//     localStorage.age = '31';
+
+//     Name = localStorage.getItem('Name');
+//     age = localStorage.getItem('age');
+
+//     console.log(Name, age);
+
+//     // deleting data from localStorage
+//     localStorage.removeItem('');
+
+//     // the following removes all the items from localstorage
+//     localStorage.clear();
+// }
+
+// 113. Stringifying & Parsing Data
 {
-    // store data in local storage
-    localStorage.setItem('Name', 'Ali');
-    localStorage.setItem('age', '32');
+    const todo = [
+        { text: 'Play mariokart', author: 'shaun' },
+        { text: 'buy milk', author: 'mario' },
+        { text: 'buy bread', author: 'luigi' }
+    ];
+    localStorage.setItem('todo', JSON.stringify(todo));
 
-    // get data from local storage and
-    let Name = localStorage.getItem('Name');
-    let age = localStorage.getItem('age');
-
-    console.log(Name, age);
-
-    // updating data
-    localStorage.setItem('Name', 'Ali Zal');
-
-    // updating data using dot notation
-    localStorage.age = '31';
-
-    Name = localStorage.getItem('Name');
-    age = localStorage.getItem('age');
-
-    console.log(Name, age);
-
-    // deleting data from localStorage
-    localStorage.removeItem('');
-
-    // the following removes all the items from localstorage
-    localStorage.clear();
+    const stored = localStorage.getItem('todo');
+    console.log(JSON.parse(stored));
 }
