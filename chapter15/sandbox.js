4106a2a3a63acf79a23e81b3c8732cc95a61f9d3
Ali <ali.zalzar@gail.com>
2021-12-06 08:11:01 +0200

    lesson 117 - Classes (introduction)

--- chapter15/sandbox.js
+++ chapter15/sandbox.js
@@ -1,32 +1,46 @@
 console.log('-------------------Section 15: Object Oriented JavaScript-------------------');
-// 116. Object Literal Recap
+// // 116. Object Literal Recap
+// {
+//     const userOne = {
+//         username: 'ali',
+//         email: 'ali@gmail.com',
+//         login() {
+//             console.log('the user logged in');
+//         },
+//         logout() {
+//             console.log('the user logged out');
+//         }
+//     };
+
+//     const userTwo = {
+//         username: 'Chun li',
+//         email: 'chun@gmail.com',
+//         login() {
+//             console.log('the user logged in');
+//         },
+//         logout() {
+//             console.log('the user logged out');
+//         }
+//     };
+
+//     console.log(userOne.email, userOne.username);
+//     userOne.login();
+//     console.log(userTwo.email, userTwo.username);
+//     userOne.login();
+
+//     const userThree = new User('Shaun@thenetninja.co.uk', 'Shaun');
+// }
+
+// 117. Classes
 {
-    const userOne = {
-        username: 'ali',
-        email: 'ali@gmail.com',
-        login() {
-            console.log('the user logged in');
-        },
-        logout() {
-            console.log('the user logged out');
-        }
-    };
-
-    const userTwo = {
-        username: 'Chun li',
-        email: 'chun@gmail.com',
-        login() {
-            console.log('the user logged in');
-        },
-        logout() {
-            console.log('the user logged out');
-        }
-    };
-
-    console.log(userOne.email, userOne.username);
-    userOne.login();
-    console.log(userTwo.email, userTwo.username);
-    userOne.login();
-
-    const userThree = new User('Shaun@thenetninja.co.uk', 'Shaun');
+    // classes are like a blue print to create an object with
+    /*  for example the following can be used as an example of 
+        blue print for creating user
+    {
+        username, // this is a property
+        email,    // this is a property
+        login(),  // this is a functionality  
+        logout(), // this is a functionality
+    }
+     */
 }
