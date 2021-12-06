38e3d81698377e791b55bb220eb8ee50a3453745
Ali <ali.zalzar@gail.com>
2021-12-06 08:21:02 +0200

    lesson 118 - Class Constructors

--- chapter15/sandbox.js
+++ chapter15/sandbox.js
@@ -31,16 +31,31 @@ console.log('-------------------Section 15: Object Oriented JavaScript----------
 //     const userThree = new User('Shaun@thenetninja.co.uk', 'Shaun');
 // }
 
-// 117. Classes
+// // 117. Classes
+// {
+//     // classes are like a blue print to create an object with
+//     /*  for example the following can be used as an example of 
+//         blue print for creating user
+//     {
+//         username, // this is a property
+//         email,    // this is a property
+//         login(),  // this is a functionality  
+//         logout(), // this is a functionality
+//     }
+//      */
+// }
+
+// 118. Class Constructors
 {
-    // classes are like a blue print to create an object with
-    /*  for example the following can be used as an example of 
-        blue print for creating user
-    {
-        username, // this is a property
-        email,    // this is a property
-        login(),  // this is a functionality  
-        logout(), // this is a functionality
+    class User {
+        constructor(tbxUsername, tbxEmail) {
+            this.username = tbxUsername;
+            this.email = tbxEmail;
+        }
     }
-     */
+
+    const userOne = new User('ali', 'ali@gmail.com');
+    const userTwo = new User('sherly', 'sherly@gmail.com');
+
+    console.log(userOne, userTwo);
 }
