d64203228a33dc57f092e435942bd839da876bff
Zalzar <ali.zalzar@gmail.com>
2021-12-21 08:22:00 +0200

    update pet project

---
+++ petProject/newtbl.js
@@ -0,0 +1,48 @@
+const content = document.querySelector('tr');
+fetch('./newtbl.json')
+    .then((response) => {
+        console.log(response, 'resolved');
+        return response.json();
+    }).then(data => {
+        html = ``;
+        console.log(data);
+        data.forEach((element, index) => {
+            html += `
+            <td colspan="${element.colspan}">
+                <!-- Modal Trigger -->
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal${index + 2}">${element.symbol} </a>
+                <!-- Modal Structure -->
+                <div id="modal${index + 2}" class="modal bottom-sheet">
+                    <div class="modal-content">
+                        <h2>${element.symbol}</h2>
+                        <h4>${element.name}</h4>
+                        <p>Energy Levels: ${element.EnergyLevels}</p>
+                        <a href="${element.source} target=" popup"><i class="material-icons">info</i></a>
+
+                        <p>Energy Levels: </p>
+                        <p>For more details, here is Wikipedia link of <a href="${element.source}">${element.name}</a>
+                        </p>
+                    </div>
+                </div>
+            </td>
+            ${element.break}
+            `;
+        });
+        content.innerHTML = html;
+        initializeModal();
+    }).catch((error) => {
+        console.log(error, ' broke the promise');
+    });
+//<a class="waves-effect waves-light btn modal-trigger" href="#modal${index + 2}" style="background-color:#${element.cpkHex};" >${element.symbol} </a>
+
+document.addEventListener('DOMContentLoaded', initializeModal);
+function initializeModal() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+// });
+
