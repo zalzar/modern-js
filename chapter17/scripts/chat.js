2292c2ffe514a63e13298be25784efbc825c04f2
Zalzar <ali.zalzar@gmail.com>
2021-12-22 08:35:25 +0200

    lesson 145. Changing Username & Local Storage

--- chapter17/scripts/chat.js
+++ chapter17/scripts/chat.js
@@ -38,6 +38,7 @@ class Chatroom {
     }
     updateName(username) {
         this.username = username;
+        localStorage.setItem('username', username);
     }
     updateRoom(room) {
         this.room = room;
@@ -47,5 +48,3 @@ class Chatroom {
         }
     }
 }
-
-
