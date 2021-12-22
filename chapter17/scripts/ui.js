3115ef8da84cd94d733d6a0f378120112f0f00b8
Zalzar <ali.zalzar@gmail.com>
2021-12-22 08:10:39 +0200

    lesson 144. Sending New Chats

--- chapter17/scripts/ui.js
+++ chapter17/scripts/ui.js
@@ -6,11 +6,14 @@ class ChatUI {
         this.list = list;
     }
     render(data) {
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(), { addSuffix: true }
+        );
         const html = `
-        <li className="list-group-item">
-            <span className="username">${data.username}</span>
-            <span className="message">${data.message}</span>
-            <div className="time">${data.created_at.toDate()}</div>
+        <li class="list-group-item">
+            <span class="username">${data.username}</span>
+            <span class="message">${data.message}</span>
+            <div class="time">${when}</div>
         </li>
         `;
         this.list.innerHTML += html;
