f4c0aa7b460fc7ce52b0eb74e7ef3606597f5056
Zalzar <ali.zalzar@gmail.com>
2022-01-03 10:30:40 +0200

    lesson 161. Watching for Changes

--- chapter19/src/data.js
+++ chapter19/src/data.js
@@ -2,6 +2,7 @@ const users = [
     { name: 'Ali', premium: true },
     { name: 'Niobe', premium: false },
     { name: 'Neo', premium: true },
+    { name: 'Link', premium: true },
     { name: 'Persephone', premium: true },
     { name: 'Morpheus', premium: true },
     { name: 'Smith', premium: false },
