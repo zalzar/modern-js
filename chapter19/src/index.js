f5a77eb1dc09433efd96a2003ba20a9d165e0cd5
zalzar <48548785+zalzar@users.noreply.github.com>
2022-01-16 09:27:43 -0800

    Delete chapter19 directory

--- chapter19/src/index.js
+++
@@ -1,7 +0,0 @@
-import { styleBody, addTitle, contact } from './dom';
-import users, { getPremiumUsers } from './data';
-
-const premUsers = getPremiumUsers(users);
-console.log(users, premUsers);
-
-console.log('test3');
